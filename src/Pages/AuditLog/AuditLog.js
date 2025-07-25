100556b3acc9dac87570fa5c4606747f463cb26f
Patrick Hoang <pxtrickhoxng@gmail.com>
2025-07-24 17:14:02 -0700

    ensure filtering takes into account 0-indexed currentPage + adjust useEffect to trigger when applying filter

--- src/Pages/AuditLog/AuditLog.js
+++ src/Pages/AuditLog/AuditLog.js
@@ -4,7 +4,6 @@ import Pagination from './Components/Pagination';
 import { useSCE } from '../../Components/context/SceContext';
 import AuditLogCard from './Components/AuditLogCard';
 import FilterActivityTypes from './Components/FilterActivityTypes';
-import RefreshButton from './Components/RefreshButton';
 import FirstNameFilter from './Components/FirstNameFilter';
 import LastNameFilter from './Components/LastNameFilter';
 
@@ -83,7 +82,6 @@ export default function AuditLogPage() {
 
   const itemsPerPage = 50;
   const totalPages = Math.ceil(auditLogsData.totalLogs / itemsPerPage);
-  const currentLogs = auditLogsData.items;
 
   const goToPage = page => {
     setCurrentPage(Math.max(0, Math.min(page, totalPages - 1)));
